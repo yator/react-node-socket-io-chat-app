5b22ef971a19537b60307c1e90df41f9c96dc48a
yator <abelkipruto4@gmail.com>
2022-04-12 21:54:46 +0300

    server setup

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require ("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const {Server} = require("socket.io");
+
+
+
+app.use(cors());
+const server= http.createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["Get","Post"]
+
+    },
+});
+
+io.on("connection",(socket)=>{
+
+    console.log(socket.id)
+
+    socket.on("disconnect",()=>{
+        console.log("disconnected",socket.id)
+
+})
+
+});
+
+server.listen(3001,()=>{
+    console.log("server running");
+});
